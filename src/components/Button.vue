24d1a79c27b0855e06f4c9fec6def67aa1067d3f
Voodoo30fps <voodoo30fps@gmail.com>
2025-11-21 17:39:04 +0200

    button vue component, button primary, secondary, play, category variants

---
+++ src/components/Button.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { defineProps, computed } from 'vue'
+import { RouterLink } from 'vue-router'
+
+const props = defineProps({
+  to: {
+    type: String,
+    default: '/',
+  },
+  variant: {
+    type: String,
+    default: 'primary', // primary secondary play
+  },
+})
+
+const link = computed(() => {
+  return props.variant === 'play' ? '/play-category' : props.to
+})
+</script>
+
+<template>
+  <RouterLink :to="link" :class="['button', props.variant]">
+    <img v-if="props.variant === 'play'" src="../assets/images/icon-play.png" alt="play-button" />
+    <slot v-else />
+  </RouterLink>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,3 +1,12 @@
-<script setup></script>
+<script setup>
+import Button from '@/components/Button.vue'
+</script>
 
-<template>the hangman game</template>
+<template>
+  <h1>the hangman game</h1>
+
+  <Button variant="play" />
+  <Button variant="primary" to="#">how to play</Button>
+  <Button variant="secondary" to="#">quit game</Button>
+  <Button variant="category" to="#">category</Button>
+</template>
